b117cf92ef51069f398de47e26fa69b803b2fe6c
nickcarva <carvalhonick2002@gmail.com>
2020-08-28 20:09:27 -0300

    Setup homepage

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Wrapper, Container, Main } from './styles'
+
+const ComingSoon: React.FC = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <Main />
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default ComingSoon
